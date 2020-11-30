4b599aa1a2a01bc9f926fbdc1455284e00e93559
Keith Shepherdson <kmson7@gmail.com>
2020-11-30 16:40:43 +0800

    add cinemas controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'cinemas/index'
+  get 'cinemas/show'
+  get 'cinemas/new'
+  get 'cinemas/create'
+  get 'cinemas/edit'
+  get 'cinemas/update'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
