0318c303519d28a546462043a2acee55c86539b4
Keith Shepherdson <kmson7@gmail.com>
2020-12-02 21:57:00 +0800

    add create and new for bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -13,11 +13,17 @@ class BookingsController < ApplicationController
   def create
     @booking = Booking.new(booking_params)
     @cinema = Cinema.find(params[:cinema_id])
-    @user = current_user
-    raise
+    @booking.cinema = @cinema
+    @booking.user = current_user
+    if @booking.save
+      #change to show after implementing show
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
-  def booking
+  def booking_params
     params.require(:booking).permit(:quantity, :date, :time)
   end
 end
