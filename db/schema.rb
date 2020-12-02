e0d55f3bef6880b9c433e2bef9506324b40900c3
Keith Shepherdson <kmson7@gmail.com>
2020-12-02 12:19:16 +0800

    reduce seeds to 5, renamed assets(yet again)

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_093611) do
+ActiveRecord::Schema.define(version: 2020_12_02_035331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "cinema_id", null: false
+    t.string "status"
+    t.string "review"
+    t.integer "rating"
+    t.integer "quantity"
+    t.date "date"
+    t.time "time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cinema_id"], name: "index_bookings_on_cinema_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "cinemas", force: :cascade do |t|
     t.string "location"
@@ -41,5 +56,7 @@ ActiveRecord::Schema.define(version: 2020_11_30_093611) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "cinemas"
+  add_foreign_key "bookings", "users"
   add_foreign_key "cinemas", "users"
 end
