c9e45e8d1de58c62f623b1f0a1cf9cd1f0d19c14
tom chang <tomlin83@gmail.com>
2020-12-03 13:55:26 +0800

    update cinemas file

--- app/models/cinema.rb
+++ app/models/cinema.rb
@@ -1,5 +1,5 @@
 class Cinema < ApplicationRecord
   belongs_to :user
-  validates :location, :capacity, :price, :description, :movie_selection, :description, :screen_size, presence: true
+  validates :location, :capacity, :price, :description, :movie_selection, :description, :screen_size, :photo, presence: true
   has_one_attached :photo
 end
