c49d25e46e4ff4f5b5803f19eb24fba04fe18dd1
Keith Shepherdson <kmson7@gmail.com>
2020-12-03 16:18:33 +0800

    add date picker

---
+++ app/javascript/plugins/init_flatpickr.js
@@ -0,0 +1,15 @@
+import flatpickr from "flatpickr";
+
+const bookingForm = () => {
+  const startDateInput = document.getElementById('booking_date');
+
+  flatpickr(startDateInput, {
+    minDate: "today",
+    altInput: true,
+    altFormat: "F j, Y",
+    dateFormat: "Y-m-d",
+    defaultDate: "today"
+  });
+};
+
+export { bookingForm };
--- app/models/cinema.rb
+++ app/models/cinema.rb
@@ -1,4 +1,6 @@
 class Cinema < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+  has_many :users, through: :bookings
   validates :location, :capacity, :price, :description, :movie_selection, :description, :screen_size, presence: true
 end
